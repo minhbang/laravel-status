49854aae6f16c4f01075fbde1535497d8edc46ac
Minh Bang MAC <contact@minhbang.com>
2018-04-29 16:20:43 +0700

    Change trans

--- tests/StatusManagerTest.php
+++ tests/StatusManagerTest.php
@@ -1,4 +1,5 @@
 <?php
+
 use App\ContentAccessControl;
 use Minhbang\Article\Article;
 
@@ -14,25 +15,22 @@ class StatusManagerTest extends TestCase
         $this->assertTrue($status->has(ContentAccessControl::STATUS_EDITING));
         $this->assertFalse($status->has(100));
 
-        $this->assertTrue($status->get('title', ContentAccessControl::STATUS_EDITING) === trans('status.editing'));
+        $this->assertTrue($status->get('title', ContentAccessControl::STATUS_EDITING) === 'Editing');
         $this->assertTrue($status->get('title1', ContentAccessControl::STATUS_EDITING, false) === false);
 
         $this->assertTrue($status->pluck() === [
-                ContentAccessControl::STATUS_EDITING   => trans('status.editing'),
-                ContentAccessControl::STATUS_REVIEWING => trans('status.reviewing'),
-                ContentAccessControl::STATUS_REFUSED   => trans('status.refused'),
-                ContentAccessControl::STATUS_PUBLISHED => trans('status.published'),
+                ContentAccessControl::STATUS_EDITING => 'Editing',
+                ContentAccessControl::STATUS_REVIEWING => 'Reviewing',
+                ContentAccessControl::STATUS_REFUSED => 'Refused',
+                ContentAccessControl::STATUS_PUBLISHED => 'Published',
             ]
         );
         $this->assertTrue($status->pluck('title', false) === [
-                trans('status.editing'),
-                trans('status.reviewing'),
-                trans('status.refused'),
-                trans('status.published'),
+                'Editing',
+                'Reviewing',
+                'Refused',
+                'Published',
             ]
         );
-
-        
-        
     }
 }
